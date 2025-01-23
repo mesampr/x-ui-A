ccb0ca782c1d21ba19135099b0c7e0f0622be8f5
ali rahimi <alirahimi818@gmail.com>
2025-01-23 21:50:44 +0100

    json post base path bug fixed

--- web/assets/js/util/utils.js
+++ web/assets/js/util/utils.js
@@ -94,7 +94,7 @@ class HttpUtil {
                 },
                 body: JSON.stringify(data),
             };
-            const resp = await fetch(url, requestOptions);
+            const resp = await fetch(basePath + url.replace(/^\/+|\/+$/g, ''), requestOptions);
             const response = await resp.json();
 
             msg = this._respToMsg({data : response});
